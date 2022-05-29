daf8bb117bae311152669e402bff13650ce6e6e1
Leonardo Vinicius <lporchat06@gmail.com>
2022-05-29 20:51:30 -0300

    move SUT creation to setup method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -21,10 +21,16 @@ abstract class HttpClient {
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  RemoteAuthentication sut;
+  HttpClientSpy httpClient;
+  String url;
+
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
+  });
   test('Shoulb call HttpClient with corrent values', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
     sut.auth();
 
     verify(httpClient.request(url: url, method: 'post'));
